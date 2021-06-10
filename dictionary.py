4fa1f4436a96c0eea2b23b19388447ad45c440a8
adityabhanu <adityabhanu08@gmail.com>
2021-06-10 08:43:04 +0530

    First Commit

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+
+    if word in data:
+        return data[word]
+    else:
+        if len(get_close_matches(word, data.keys(), 3, 0.8)) > 0:
+            yn = input("Did you mean %s instead? Enter Y if yes or N for no: " % get_close_matches(word, data.keys(), 3, 0.8)[0])
+            if yn.lower() == 'y':
+                return data[get_close_matches(word, data.keys(), 3, 0.8)[0]]
+            elif yn.lower() == 'n':
+                return "No matches found."
+            else:
+                return "we didn't understand your entry"
+
+        return "No matches found"
+
+user_input = input("Enter a word: ")
+
+definition = translate(user_input)
+
+if type(definition) == list:
+    for idx, item in enumerate(definition):
+        print(f"{idx}. {item}")
+else:
+    print(definition)
